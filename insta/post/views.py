5b53a1cb9d8a31dcdd8cf87da01df8053612b781
ssa <wogur981208@gmail.com>
2022-10-02 03:37:40 +0900

    Add Post

--- insta/post/views.py
+++ insta/post/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "post/index.html")
+
+def detail(request):
+    return render(request, "post/post_detail.html")
