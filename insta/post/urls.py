d1c920e0b7f17b24833e2cbaa18da32b3b5d41ec
ssa <wogur981208@gmail.com>
2022-10-03 13:26:57 +0900

    Add profile update

--- insta/post/urls.py
+++ insta/post/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     #profile urls
     path("users/<int:user_id>/", views.ProfileView.as_view(), name="profile"),
     path("set-profile/",views.ProfileSetView.as_view(), name="profile-set"),
+    path("edit-profile/",views.ProfileUpdateView.as_view(), name="profile-update"),
 ]
--- insta/user/views.py
+++ insta/user/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
+from braces.views import LoginRequiredMixin
 from allauth.account.views import PasswordChangeView
 
 # Create your views here.
 
-class CustomPasswordChangeView(PasswordChangeView):
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     def get_success_url(self):
-        return reverse("index")
+        return reverse("profile", kwargs={"user_id":self.request.user.id })
     
